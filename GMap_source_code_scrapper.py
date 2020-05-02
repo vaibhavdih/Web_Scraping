97881a24b2c06ac920edad5ee4f4fe284bbcdb17
Gagan-IIT <44469150+Gagan-IIT@users.noreply.github.com>
2020-05-02 11:26:16 +0530

    Created Source code scraper for Google Maps url
    This code scrapes using Selenium.

---
+++ GMap_source_code_scrapper.py
@@ -0,0 +1,23 @@
+'''
+This code scrapes the source code of google maps url using selenium 
+while going to every page using the next button and dumps it to the
+output console.
+'''
+
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get('https://www.google.com/maps/search/medical+shops+in+varanasi/');
+time.sleep(10)
+
+button = driver.find_elements_by_xpath('//*[@id=\"n7lv7yjyC35__section-pagination-button-next\"]/span')[0]
+
+while(True):
+    try:
+        print(driver.page_source, "\n=======================================\n")
+        button.click()
+        time.sleep(2)
+    except:
+        print('---Finished---')
+        break
